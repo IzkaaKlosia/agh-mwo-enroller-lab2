0914878925a1f3e2058150fbf8f0aa914b8fd9bc
Iza Klosinska <klosinskaiza@gmail.com>
2023-04-21 18:05:34 +0200

    meetings

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +22,13 @@ public class MeetingService {
 		Query query = this.session.createQuery(hql);
 		return query.list();
 	}
+	public Meeting findById(long id) {
+		return this.session.get(Meeting.class, id);
+	}
 
+	public void add(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.save(meeting);
+		transaction.commit();
+	}
 }
